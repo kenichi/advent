9aa8ee73a8e5c7ee4592798d3a3daf948497bfab
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2018-12-03 12:47:26 -0800

    day2-part2

--- 02/part2/lib/advent.ex
+++ 02/part2/lib/advent.ex
@@ -8,9 +8,24 @@ defmodule Advent do
   end
 
   def common(list \\ read_input()) do
+    Enum.reduce(list, "", fn s, common ->
+      case find_match(list, s) do
+        nil -> common
+        m ->
+          String.myers_difference(s, m)
+          |> Enum.reduce("", fn {op,v}, str -> if op == :eq, do: str <> v, else: str end)
+      end
+    end)
   end
 
   def find_match(list, s) do
+    l = for l <- list, count_eqs(l, s) == (String.length(s) - 1), do: l
+    if length(l) > 0, do: hd(l), else: nil
+  end
+
+  def count_eqs(a, b) do
+    String.myers_difference(a, b)
+    |> Enum.reduce(0, fn {op,v}, c -> if op == :eq, do: String.length(v) + c, else: c end)
   end
 
 end
--- 02/part2/test/advent_test.exs
+++ 02/part2/test/advent_test.exs
@@ -29,4 +29,23 @@ defmodule AdventTest do
     assert Advent.read_input(dev) |> Advent.common == "fgij"
   end
 
+  test "find_match" do
+    {:ok, dev} = StringIO.open(@input)
+    list = Advent.read_input(dev) 
+    assert Advent.find_match(list, "abcde") == nil
+    assert Advent.find_match(list, "fghij") == "fguij"
+    assert Advent.find_match(list, "klmno") == nil
+    assert Advent.find_match(list, "pqrst") == nil
+    assert Advent.find_match(list, "fguij") == "fghij"
+    assert Advent.find_match(list, "axcye") == nil
+    assert Advent.find_match(list, "wvxyz") == nil
+  end
+
+  test "count_eqs" do
+    assert Advent.count_eqs("abcde", "abzdf") == 3
+    assert Advent.count_eqs("abcde", "abcdf") == 4
+    assert Advent.count_eqs("abcde", "abcde") == 5
+    assert Advent.count_eqs("abcde", "edcba") == 1
+  end
+
 end
