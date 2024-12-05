07b525686440ba5211c933e2749adc104418f1bd
Kenichi Nakamura <kenichi@ratiopbc.com>
2024-12-05 08:33:50 -0800

    add dialyzer

--- 2024/05/part2/mix.exs
+++ 2024/05/part2/mix.exs
@@ -30,6 +30,7 @@ defmodule Advent.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
